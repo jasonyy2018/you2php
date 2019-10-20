7a383d5996a8ddaf1833e7a42072f69c5af8e3eb
136ytr <44334104+136ytr@users.noreply.github.com>
2019-10-20 14:30:09 +0800

    Add files via upload

---
+++ web/inc/config.php
@@ -0,0 +1,10 @@
+<?php
+$gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
+define('ROOT_PART', Root_part());
+define('APIKEY', 'AIzaSyC2mp6eD1vucTgiH5lbkr5WcE8mZuHe6Fo');
+define('GJ_CODE', $gl);
+define('SITE_NAME', 'JJ5CM');
+define('TITLENAME', 'JJ5CM');
+define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
+define('EMAIL', 'justsweetpotato@gmail.com');
+?>
