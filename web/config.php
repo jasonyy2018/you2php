864589c022be5268c2e2d74ab8c9d4488f4c8c9e
136ytr <44334104+136ytr@users.noreply.github.com>
2019-10-20 14:43:31 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyByudd09CeGwJDAUsKrsO_ThY3QkxiDufE');
 define('GJ_CODE', $gl);
 define('SITE_NAME', 'Hello World!');
 define('TITLENAME', 'Hello World!');
-define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
+define('EN2DEKEY', 'AnQUP1XDEF87ewemh58-=--/4:CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
